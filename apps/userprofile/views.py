88d08ba26cb786c3b4b76300fd0331490088f697
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-10-07 17:23:03 -0400

    templates

--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -1,10 +1,29 @@
+from django.contrib import messages
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
 
 from .models import UserProfile
 
+@login_required
+def myaccount(request):
+    return render(request, 'myaccount.html')
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        print(f"User: {request.user}")
+        request.user.first_name = request.POST.get('first_name', '')
+        request.user.last_name = request.POST.get('last_name', '')
+        request.user.email = request.POST.get('email', '')
+        request.user.save()
+
+        messages.info(request, 'Successfully updated account')
+
+        return redirect('myaccount')
+
+    return render(request,'edit_profile.html')
 
 def signup(request):
     if request.method == 'POST':
