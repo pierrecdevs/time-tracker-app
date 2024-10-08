de4118b3c64b1b33bc4cc264e183448d14a0b3d0
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-10-08 02:06:31 -0400

    add Userprofile to admin site

--- apps/userprofile/admin.py
+++ apps/userprofile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from apps.userprofile.models import UserProfile
+
 # Register your models here.
+from .models import UserProfile
+
+admin.site.register(UserProfile)
