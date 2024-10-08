eca12edf119f164e8f790e31bdc120562d780bfa
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-10-08 18:34:16 -0400

    project templates

---
+++ apps/project/views.py
@@ -0,0 +1,50 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect, get_object_or_404
+
+# Create your views here.
+
+from .models import Project
+from apps.team.models import Team
+
+@login_required
+def projects(request):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    projects = team.projects.all()
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+
+        if title:
+            project = Project.objects.create(team=team, title=title, created_by=request.user)
+            messages.info(request, f'Project "{title}" has been added.')
+
+            return redirect('project:projects')
+
+    return render(request, 'project/projects.html', {'team': team, 'projects': projects})
+
+@login_required
+def project(request, project_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+
+    return render(request, 'project/project.html', {'team': team, 'project': project})
+
+@login_required
+def edit_project(request, project_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+
+
+        if title:
+            project.title = title
+            project.save()
+
+            messages.info(request, 'Project has been updated.')
+
+            return redirect('project:project', project_id=project.id)
+
+    return render(request, 'project/edit_project.html', {'team': team, 'project': project})
