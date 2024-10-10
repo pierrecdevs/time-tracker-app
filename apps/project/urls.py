cc20d35875d52e7d7be0ee834070014f6ad6ec36
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-10-09 20:49:47 -0400

    setup views and urls

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from .views import project, projects, edit_project
+from .views import delete_entry, edit_entry, edit_task, project, projects, edit_project, task
 
 app_name = 'project'
 
+ # TODO:
+ # change the way the CRUD operations work. (i.e use proper GET, POST, PATCH/PUT, DELETE)
 urlpatterns = [
     path('', projects, name='projects'),
     path('<int:project_id>/', project, name='project'),
+    path('<int:project_id>/<int:task_id>/', task, name='task'),
+    path('<int:project_id>/<int:task_id>/edit/', edit_task, name='edit_task'),
+    path('<int:project_id>/<int:task_id>/<int:entry_id>/edit/', edit_entry, name='edit_entry'),
+    path('<int:project_id>/<int:task_id>/<int:entry_id>/delete/', delete_entry, name='delete_entry'),
     path('<int:project_id>/edit/', edit_project, name='edit_project')
 ]
--- apps/project/views.py
+++ apps/project/views.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect, get_object_or_404
 
 # Create your views here.
 
-from .models import Project
+from .models import Entry, Project, Task
 from apps.team.models import Team
 
 @login_required
@@ -28,7 +29,19 @@ def project(request, project_id):
     team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
     project = get_object_or_404(Project, team=team, pk=project_id)
 
-    return render(request, 'project/project.html', {'team': team, 'project': project})
+    if request.method == 'POST':
+        title = request.POST.get('title')
+
+        if title:
+            task = Task.objects.create(team=team, project=project, created_by=request.user,  title=title)
+
+            messages.info(request, 'Task has been added.')
+            return redirect('project:project', project_id=project.id)
+
+    tasks_todo = project.tasks.filter(status=Task.TODO)
+    tasks_done = project.tasks.filter(status=Task.DONE)
+
+    return render(request, 'project/project.html', {'team': team, 'project': project, 'tasks_todo': tasks_todo, 'tasks_done': tasks_done})
 
 @login_required
 def edit_project(request, project_id):
@@ -48,3 +61,87 @@ def edit_project(request, project_id):
             return redirect('project:project', project_id=project.id)
 
     return render(request, 'project/edit_project.html', {'team': team, 'project': project})
+
+
+@login_required
+def task(request,project_id, task_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id, team=team)
+
+    if request.method == 'POST':
+        hours = int(request.POST.get('hours', 0))
+        minutes = int(request.POST.get('minutes', 0))
+        date = f'{request.POST.get('date')} {datetime.now().time()}'
+        minutes_total = (hours * 60) + minutes
+
+        entry = Entry.objects.create(team=team, project=project, task=task, minutes=minutes_total, created_by=request.user, created_at=date)
+
+    return render(request, 'project/task.html', {'today': datetime.today,'team': team, 'project': project, 'task': task})
+
+@login_required
+def edit_task(request,project_id, task_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id, team=team)
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        status = request.POST.get('status')
+
+        if title:
+            task.title = title
+            task.status = status
+            task.save()
+
+            messages.info(request, 'Task has been updated.')
+            return redirect('project:task', project_id=project.id, task_id=task.id)
+
+    return render(request, 'project/edit_task.html', {'team': team, 'project': project, 'task': task})
+
+
+@login_required
+def edit_entry(request, project_id, task_id, entry_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id, team=team)
+    entry = get_object_or_404(Entry, pk=entry_id, team=team)
+
+    if request.method == 'POST':
+        hours = int(request.POST.get('hours', 0))
+        minutes = int(request.POST.get('minutes',0))
+
+        date = f'{request.POST.get('date')} {datetime.now().time()}'
+
+        entry.created_at = date
+        entry.minutes = (hours * 60) + minutes
+        entry.save()
+
+        messages.info(request, 'Entry has been updated.')
+
+        return redirect('project:task', project_id=project.id, task_id=task.id)
+
+    hours, minutes = divmod(entry.minutes, 60)
+
+    context = {
+        'team': team,
+        'project': project,
+        'task': task,
+        'entry': entry,
+        'hours': hours,
+        'minutes': minutes,
+    }
+
+    return render(request, 'project/edit_entry.html', context)
+
+@login_required
+def delete_entry(request, project_id, task_id, entry_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id, team=team)
+    entry = get_object_or_404(Entry, pk=entry_id, team=team)
+    entry.delete()
+    messages.info(request, 'Removed successfully.')
+
+    return redirect('project:task', project_id=project.id, task_id=task.id)
+
