69a4dcd1b88809a1fab54ac83503679ca92e9c5b
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-10-10 11:49:27 -0400

    update templates and views

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 
-from apps.dashboard.utilities import get_time_for_user_and_date, get_time_for_user_and_month, get_time_for_user_and_project_and_month
+from apps.dashboard.utilities import get_time_for_team_and_month, get_time_for_user_and_date, get_time_for_user_and_month, get_time_for_user_and_project_and_month, get_time_for_user_and_team_month
 from apps.project.models import Entry
 from apps.team.models import Team
 
@@ -25,6 +25,7 @@ def dashboard(request):
     date_entries = Entry.objects.filter(team=team, created_by=request.user, created_at__date=date_user, is_tracked=True)
 
 
+    # User month, pagination
     user_num_months = int(request.GET.get('user_num_months', 0))
     user_month = datetime.now() - relativedelta(month=user_num_months)
     for project in all_projects:
@@ -34,9 +35,19 @@ def dashboard(request):
                 request.user,
                 user_month,
         )
+
+    # Team month, pagination
+    team_num_months = int(request.GET.get('team_num_months', 0))
+    team_month = datetime.now() - relativedelta(months=team_num_months)
+
+    for member in members:
+        member.time_for_user_and_team_and_month = get_time_for_user_and_team_month(team, member, team_month)
+
+
     context = {
         'team': team,
         'all_projects': all_projects,
+        'projects': all_projects[0:4],
         'date_entries': date_entries,
         'num_days': num_days,
         'members': members,
@@ -44,6 +55,9 @@ def dashboard(request):
         'user_month': user_month,
         'time_for_user_and_month': get_time_for_user_and_month(team, request.user, user_month),
         'time_for_user_and_date': get_time_for_user_and_date(team, request.user, date_user),
+        'time_for_team_and_month': get_time_for_team_and_month(team, team_month),
+        'team_num_months': team_num_months,
+        'team_month': team_month,
     }
 
     return render(request, 'dashboard/dashboard.html', context=context)
