df762b72cbd346b5a3b635f7c7532abea184d691
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-10-24 17:03:14 -0400

    registe with admin site

--- apps/team/admin.py
+++ apps/team/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Invitation, Team
+from .models import Invitation, Plan, Team
 
 admin.site.register(Team)
 admin.site.register(Invitation)
+admin.site.register(Plan)
