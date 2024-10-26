f2167692129f86c357fb4ebc3d37a63195ec065f
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-10-26 11:38:13 -0400

    update the urls

--- apps/team/urls.py
+++ apps/team/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import activate_team, add, invite, plans, team, edit
+from .views import activate_team, add, invite, plans, plans_thankyou, team, edit
+from .api import stripe_webhook, create_checkout_session
 
 app_name = 'team'
 
@@ -10,4 +11,8 @@ urlpatterns = [
     path('activate_team/<int:team_id>/', activate_team, name='activate_team'),
     path('<int:team_id>/', team, name='team'),
     path('plans/', plans, name='plans'),
+    path('plans/thank_you/', plans_thankyou, name='plans_thankyou'),
+
+    path('api/stripe_webhook/', stripe_webhook, name='stripe_webhook'),
+    path('api/create_checkout_session/', create_checkout_session, name='create_checkout_session'),
 ]
